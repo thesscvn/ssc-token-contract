df9785f0eadbf200d84d71bab58d56d5d9bd1879
Mona Lisa Developer <gialong030198@gmail.com>
2021-11-11 14:49:33 +0700

    init code

--- contracts/SSC.sol
+++ contracts/SSC.sol
@@ -6,21 +6,49 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+abstract contract BPContract {
+  function protect(
+    address sender,
+    address receiver,
+    uint256 amount
+  ) external virtual;
+}
+
 contract SSC is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
-    constructor() ERC20("SSC", "SSC") {
-        _mint(msg.sender, 100000000 * 10 ** decimals());
-    }
+  BPContract public BP;
+  bool public bpEnabled;
+  bool public BPDisabledForever = false;
 
-    function snapshot() public onlyOwner {
-        _snapshot();
-    }
+  constructor() ERC20("SSC", "SSC") {
+    _mint(msg.sender, 100000000 * 10**decimals());
+  }
 
-    // The following functions are overrides required by Solidity.
+  function snapshot() public onlyOwner {
+    _snapshot();
+  }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Snapshot)
-    {
-        super._beforeTokenTransfer(from, to, amount);
+  function setBPAddrss(address _bp) external onlyOwner {
+    require(address(BP) == address(0), "Can only be initialized once");
+    BP = BPContract(_bp);
+  }
+
+  function setBpEnabled(bool _enabled) external onlyOwner {
+    bpEnabled = _enabled;
+  }
+
+  function setBotProtectionDisableForever() external onlyOwner {
+    require(BPDisabledForever == false);
+    BPDisabledForever = true;
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20, ERC20Snapshot) {
+    if (bpEnabled && !BPDisabledForever) {
+      BP.protect(from, to, amount);
     }
+    super._beforeTokenTransfer(from, to, amount);
+  }
 }
